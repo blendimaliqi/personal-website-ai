d9d8486a6ba90873d45d06fb13d0104a4afc96c7
Blendi Maliqi <55741869+blendimaliqi@users.noreply.github.com>
2024-08-21 20:02:10 +0200

    removed footer for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           <div className="flex min-h-screen flex-col">
             <HeaderClient />
             <main className="flex-grow">{children}</main>
-            <Footer />
+            {/* <Footer /> */}
           </div>
         </ThemeProvider>
         <script
