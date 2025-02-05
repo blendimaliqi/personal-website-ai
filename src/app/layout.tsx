6ca5e2f21ecf11a23b983fa8c8ec59206baaafdd
Blendi Maliqi <55741869+blendimaliqi@users.noreply.github.com>
2025-02-05 20:32:46 +0100

    removed border bottom on nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -134,7 +134,7 @@ export default function RootLayout({
         >
           <div className="flex min-h-screen flex-col">
             <HeaderClient />
-            <main className="flex-grow px-4 sm:px-6 lg:px-8">{children}</main>
+            <main className="flex-1 px-4 sm:px-6 lg:px-8">{children}</main>
             <Footer />
           </div>
         </ThemeProvider>
